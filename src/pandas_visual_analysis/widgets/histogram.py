6969dd1fb351af392cb27b7f207c6592dba12995
Matthias Matt <e1529399@student.tuwien.ac.at>
2020-09-15 13:26:38 +0200

    implements histogram selection; note: there is still a bug when selecting with multiple traces

--- src/pandas_visual_analysis/widgets/histogram.py
+++ src/pandas_visual_analysis/widgets/histogram.py
@@ -50,12 +50,24 @@ class HistogramWidget(BaseWidget):
         self.data = self.data_source.data
         self.brushed_data = self.data_source.brushed_data
 
-        self.figure_widget = self._get_figure_widget()
+        self.figure = self._get_histograms()
+        self.figure_widget = go.FigureWidget(self.figure)
 
         self.set_observers()
         self.column_select.observe(handler=self._on_column_change, names="value")
         self.normalize.observe(handler=self._on_normalize_change, names="value")
+        self.figure_widget.data[0].on_selection(callback=self.on_selection)
         self.figure_widget.data[0].on_deselect(callback=self.on_deselection)
+        self.selection_initiated = (
+            True  # set to True for _redraw_plot in order to set values correctly
+        )
+        self._redraw_plot(
+            only_brushed=True,
+            selection_reset=(
+                len(self.data_source.brushed_indices) == len(self.data_source)
+            ),
+        )
+        self.selection_initiated = False
 
     def build(self) -> widgets.Widget:
         root = widgets.VBox(
@@ -65,27 +77,28 @@ class HistogramWidget(BaseWidget):
 
     def observe_brush_indices_change(self, sender):
         self.brushed_data = self.data_source.brushed_data
-        if len(self.brushed_data) == len(self.data):
-            self.figure_widget.data[0].visible = False
-        else:
-            self.figure_widget.data[0].visible = True
+        selection_reset = len(self.brushed_data) == len(self.data)
+
+        self._redraw_plot(selection_reset=selection_reset)
 
         # empty selection for histogram does not work
-        if len(self.brushed_data) == 0:
+        if len(self.data_source.brushed_indices) == 0:
+            print("ATTENTION: setting visibility of data[1] to false")
+            print("brushed_indices:", self.data_source.brushed_indices)
             self.figure_widget.data[1].visible = False
-        else:
-            self.figure_widget.data[1].visible = True
 
         self.figure_widget.data[
             1
         ].selectedpoints = (
             self.data_source.brushed_indices
         )  # set selected points so that double click works
-        self._redraw_plot()
 
-    # issue: selection does not work for histogram: https://github.com/plotly/plotly.py/issues/2698
+        self.selection_initiated = False
+
     def on_selection(self, trace, points, state):
-        pass
+        self.selection_initiated = True
+        print("SELECTED POINTS:", points.point_inds)
+        self.data_source.brushed_indices = points.point_inds
 
     def on_deselection(self, trace, points):
         self.data_source.reset_selection()
@@ -108,7 +121,7 @@ class HistogramWidget(BaseWidget):
         config = Config()
         fig = go.Figure(layout=go.Layout(margin=dict(l=5, r=5, b=5, t=5, pad=2)))
         fig.add_trace(
-            go.Histogram(
+            go.Histogram(  # ALL DATA: 0
                 x=self.data[col],
                 opacity=max(config.alpha, 0.75),
                 marker={"color": "rgb(%d,%d,%d)" % config.deselect_color},
@@ -116,11 +129,11 @@ class HistogramWidget(BaseWidget):
                 unselected={"marker": {"opacity": 0.4}},
                 hoverinfo="skip",
                 histnorm="",
-                bingroup=1,
+                bingroup="1",
             )
         )
         fig.add_trace(
-            go.Histogram(
+            go.Histogram(  # SELECTED DATA: 1
                 x=self.brushed_data[col],
                 opacity=1.0,
                 # mode='markers',
@@ -129,15 +142,52 @@ class HistogramWidget(BaseWidget):
                 unselected={"marker": {"opacity": 1.0}},
                 hoverinfo="skip",
                 histnorm="",
-                bingroup=1,
+                bingroup="1",
             )
         )
         fig.update_layout(barmode="overlay", showlegend=False, dragmode="select")
         return fig
 
-    def _redraw_plot(self, only_brushed=True):
+    def _redraw_plot(self, only_brushed=True, selection_reset=False):
         col = self.column_select.value
         with self.figure_widget.batch_update():
-            self.figure_widget.data[1].x = self.brushed_data[col]
             if not only_brushed:
                 self.figure_widget.data[0].x = self.data[col]
+            if self.selection_initiated or selection_reset:
+                self._set_directly_selected()
+                print("directly selected")
+                # self.figure_widget.data[0].selectedpoints = self.data_source.brushed_indices
+            else:
+                self._set_externally_selected()
+                print("externally selected")
+                self.figure_widget.data[0].selectedpoints = self.data_source.indices
+                self.figure_widget.data[1].x = self.brushed_data[col]
+
+    def _set_directly_selected(self):
+        self.figure_widget.data[0].visible = True
+        self.figure_widget.data[0].marker = {
+            "color": "rgb(%d,%d,%d)" % Config().select_color
+        }
+        self.figure_widget.data[0].unselected = {
+            "marker": {
+                "opacity": Config().alpha,
+                "color": "rgb(%d,%d,%d)" % Config().deselect_color,
+            }
+        }
+        self.figure_widget.data[0].selected = {
+            "marker": {"color": "rgb(%d,%d,%d)" % Config().select_color}
+        }
+        self.figure_widget.data[0].opacity = 1.0
+        self.figure_widget.data[1].visible = False
+
+    def _set_externally_selected(self):
+        self.figure_widget.data[0].visible = True
+        self.figure_widget.data[0].marker = {
+            "color": "rgb(%d,%d,%d)" % Config().deselect_color
+        }
+        self.figure_widget.data[0].unselected = {"marker": {"opacity": 0.4}}
+        self.figure_widget.data[0].selected = {
+            "marker": {"color": "rgb(%d,%d,%d)" % Config().deselect_color}
+        }
+        self.figure_widget.data[0].opacity = max(Config().alpha, 0.75)
+        self.figure_widget.data[1].visible = True
